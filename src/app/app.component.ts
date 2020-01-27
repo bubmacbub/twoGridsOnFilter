43eba1d80d5a6a332e451b21718a5f842119c9ed
Monty Jordan III <monty.jordan@its.ny.gov>
2020-01-26 20:09:06 -0500

    Telerik's search all example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { products } from './products';
+import { DataBindingDirective } from '@progress/kendo-angular-grid';
+import { process } from '@progress/kendo-data-query';
+import { employees } from './employees';
+import { images } from './images';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'TwoGridsThatFilterEachOther';
+export class AppComponent implements OnInit {
+  @ViewChild(DataBindingDirective, { static: false }) dataBinding: DataBindingDirective;
+  title: string = "Two Kendo Grids Filtering Each Other"
+  public gridData: any[] = employees;
+  public gridView: any[];
+
+  public mySelection: string[] = [];
+
+  public ngOnInit(): void {
+    this.gridView = this.gridData;
+  }
+
+
+
+  public onFilter(inputValue: string): void {
+    console.log("On Filter method got passed =>", inputValue);
+    
+    let processedDataResult = process(this.gridData, {
+      filter: {
+        logic: "or",
+        filters: [
+          {
+            field: 'full_name',
+            operator: 'contains',
+            value: inputValue
+          },
+          {
+            field: 'job_title',
+            operator: 'contains',
+            value: inputValue
+          },
+          {
+            field: 'budget',
+            operator: 'contains',
+            value: inputValue
+          },
+          {
+            field: 'phone',
+            operator: 'contains',
+            value: inputValue
+          },
+          {
+            field: 'address',
+            operator: 'contains',
+            value: inputValue
+          }
+        ],
+      }
+    });
+    console.log("Data result from process", processedDataResult)
+
+    this.gridView = processedDataResult.data;
+    this.dataBinding.skip = 0;
+  }
+
+
+  private photoURL(dataItem: any): string {
+    const code: string = dataItem.img_id + dataItem.gender;
+    const image: any = images;
+
+    return image[code];
+  }
+
+  private flagURL(dataItem: any): string {
+    const code: string = dataItem.country;
+    const image: any = images;
+
+    return image[code];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,39 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { GridModule,PDFModule, ExcelModule } from '@progress/kendo-angular-grid';
+
+import { ChartsModule } from '@progress/kendo-angular-charts';
+import 'hammerjs';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+
+import { RatingComponent } from './rating.component';
+// import { PDFExportModule } from '@progress/kendo-angular-pdf-export';
+
+
+
+
+
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,HttpClientModule,
+    AppRoutingModule,
+    GridModule,
+    ExcelModule,
+    
+    ChartsModule,
+    InputsModule,
+    PDFModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  declarations: [AppComponent, RatingComponent],
 })
 export class AppModule { }
